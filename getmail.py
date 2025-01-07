a48ce2d2076adde05d09f5e4758d6abecddc82f6
tetsuya-ki <64536338+tetsuya-ki@users.noreply.github.com>
2025-01-07 21:21:52 +0900

    fix #1
    - multipartのやつもなんとなく対応(Copilotが教えてくれた)

--- getmail.py
+++ getmail.py
@@ -44,8 +44,9 @@ class GetMail:
 
                 # 件名に想定の文章が含まれていない場合はスキップ
                 mail_content_subject = mail_content.get('subject')
-                print(mail_content_subject)
+                print(f'mail_content_subject:{mail_content_subject}')
                 if not mail_content_subject:
+                    print(f'mail_content_subject is None')
                     continue
                 if settings.TARGET_SUBJECT:
                     print('TARGET_SUBJECT: ' + settings.TARGET_SUBJECT)
@@ -67,53 +68,75 @@ class GetMail:
 
     # 本文を取得
     def get_content(self, msg):
-        charset = msg.get_content_charset()
-        payload = msg.get_payload(decode=True)
-        print(f'charset: {charset}')
-        # print(f'payload: {payload}')
-        res = payload
+        payload = None
         try:
-            if payload:
-                if charset:
-                    res = payload.decode(charset, 'ignore')
-                    res = res.replace('\u3000', '  ')
-                    print(f'payload(decode by charset): {res}')
-                    return res
+            if msg.is_multipart() is False:
+                charset = msg.get_content_charset()
+                payload = msg.get_payload(decode=True)
+                print(f'charset: {charset}')
+                # print(f'payload: {payload}')
+                res = payload
+                if payload:
+                    if charset:
+                        res = payload.decode(charset, 'ignore')
+                        res = res.replace('\u3000', '  ')
+                        print(f'payload(decode by charset): {res}')
+                        return res
+                    else:
+                        res = payload.decode()
+                        print(f'payload(decode): {res}')
+                        return res
                 else:
-                    res = payload.decode()
-                    print(f'payload(decode): {res}')
-                    return res
+                    print('payload is ""')
+                    return ""
             else:
-                print('payload is ""')
-                return ""
+                res = ''
+                for part in msg.walk():
+                    charset = part.get_content_charset()
+                    payload = part.get_payload(decode=True)
+                    print(f'charset: {charset}')
+                    # print(f'payload: {payload}')
+                    if payload:
+                        if charset:
+                            tmp = payload.decode(charset, 'ignore')
+                            print(f'payload(decode): {tmp}')
+                            res += tmp.replace('\u3000', '  ')
+                        else:
+                            tmp = payload.decode()
+                            print(f'payload(decode): {tmp}')
+                            res += tmp.replace('\u3000', '  ')
+                    else:
+                        print('payload is ""')
+                print(f'final payload: {res}')
+                return res
         except:
             print('payload is not decode')
             return payload
 
-    def bunbetu (self, content:str='')->dict:
+    def bunbetu(self, content:str='')->dict:
         # こういう文章がくるパターンで考えている(違う場合は適宜直してください)
-        text = '''
-________________________________________
-差出人: XXXXX<XXXXX@XXXXX.XXXXX.com>
-送信日時: 2024年12月20日 2:48
-宛先: XXXXX@outlook.jp <XXXXX@outlook.jp>
-件名: Misskey通知(仮)
+#         text = '''
+# ________________________________________
+# 差出人: XXXXX<XXXXX@XXXXX.XXXXX.com>
+# 送信日時: 2024年12月20日 2:48
+# 宛先: XXXXX@outlook.jp <XXXXX@outlook.jp>
+# 件名: Misskey通知(仮)
 
-date: Friday, December 20, 2024 11:43:15 AM
-General お疲れさまです   XXXXXの際にXXXXXを配布する予定です。ついては、配布総数を確認したいので、XXXXXが欲しい方は必要数を投稿してください。   アンケート回答期限：本日15時まで   XXXXX必要数確認アンケート [https://forms.office.com/r/XXXXX]     XXXXX不参加の方でもご希望があれば確保します。   ただし、現在の在庫数XXXXX冊のため、必要数が在庫数を上回る場合には、XXXXX＞XXXXX＞XXXXXの優先順で数量を調整させていただきます。
+# date: Friday, December 20, 2024 11:43:15 AM
+# General お疲れさまです   XXXXXの際にXXXXXを配布する予定です。ついては、配布総数を確認したいので、XXXXXが欲しい方は必要数を投稿してください。   アンケート回答期限：本日15時まで   XXXXX必要数確認アンケート [https://forms.office.com/r/XXXXX]     XXXXX不参加の方でもご希望があれば確保します。   ただし、現在の在庫数XXXXX冊のため、必要数が在庫数を上回る場合には、XXXXX＞XXXXX＞XXXXXの優先順で数量を調整させていただきます。
 
-Mail From V2
-        '''
+# Mail From V2
+#         '''
 
-        if content == '':
-            content = text
+        # if content == '':
+        #     content = text
 
         # 上記文章から件名、date、本文を取得
         subject,date = None,None
         content_below_date = ''
         for line in content.split('\n'):
-            if line.startswith('件名:'):
-                subject = line.replace('件名:', '').strip()
+            if line.startswith('Subject:'):
+                subject = line.replace('Subject:', '').strip()
             elif line.startswith('date:'):
                 date = line.replace('date:', '').strip()
         if date:
